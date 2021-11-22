083019bfeac14423eb7f1a8c7b81c1c9ad8f59bd
chgray@gmail.com <Chris Gray>
2021-11-22 12:16:45 -0800

    generally ok

--- keyboards/log0/yeet30/keymaps/vial/config.h
+++ keyboards/log0/yeet30/keymaps/vial/config.h
@@ -2,17 +2,15 @@
 
 #define VIAL_KEYBOARD_UID {0x59, 0x52, 0xBB, 0x79, 0xCB, 0x23, 0xE5, 0xC6}
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
 
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 #define DISABLE_LEADER
 
-
 #ifndef NO_PRINT
 #error "ERROR : NO_PRINT should be set so the image fits"
 #endif
-
 
 #ifndef NO_DEBUG
 #error "ERROR : no_debug should be set so the image fits"
