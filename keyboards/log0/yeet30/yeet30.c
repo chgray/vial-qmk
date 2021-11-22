e7752089744274ec91dc207c10df5d3dc8f3da0f
chgray@gmail.com <Chris Gray>
2021-11-22 09:13:39 -0800

    mostly ok

--- keyboards/log0/yeet30/yeet30.c
+++ keyboards/log0/yeet30/yeet30.c
@@ -2,11 +2,17 @@
 
 
 #ifdef RGB_MATRIX_ENABLE
+
+
+#define RGBLIGHT_LED_MAP { 3, 2, 1, 0 }
+
+//https://docs.qmk.fm/#/feature_rgb_matrix?id=common-configuration
+
 led_config_t g_led_config = { {
-    { 0 }
+    { 17, 18, 52, 53, 83, 72 }
 }, {
-    { 0 }
+    { 0,   0 }, { 15,   0 }, { 30,   0 }, { 45,   0 }, { 60,   0 }, { 75,   0 }
 }, {
-    1
+    1, 1, 1, 1, 1, 1
 } };
 #endif
